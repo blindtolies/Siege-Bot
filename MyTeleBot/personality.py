9933bb4d63b45f4310434c56df8ecfe278c47a5f
blindtolies <briankstephenson83@gmail.com>
2025-10-22 01:42:17 -0400

    Update personality.py

--- MyTeleBot/personality.py
+++ MyTeleBot/personality.py
@@ -1,6 +1,5 @@
 import random
 import re
-import wikipedia
 import logging
 
 class SiegePersonality:
@@ -83,67 +82,6 @@ class SiegePersonality:
             "💀", "⚔️", "🤖", "😤", "🔥", "⚡", "💯", "🎯", "👑", "🗿"
         ]
 
-    def get_periodic_element(self, atomic_number: int) -> str:
-        """Get element info by atomic number"""
-        elements = {
-            1: "Hydrogen (H)", 2: "Helium (He)", 3: "Lithium (Li)", 4: "Beryllium (Be)", 5: "Boron (B)",
-            6: "Carbon (C)", 7: "Nitrogen (N)", 8: "Oxygen (O)", 9: "Fluorine (F)", 10: "Neon (Ne)",
-            11: "Sodium (Na)", 12: "Magnesium (Mg)", 13: "Aluminum (Al)", 14: "Silicon (Si)", 15: "Phosphorus (P)",
-            16: "Sulfur (S)", 17: "Chlorine (Cl)", 18: "Argon (Ar)", 19: "Potassium (K)", 20: "Calcium (Ca)",
-            21: "Scandium (Sc)", 22: "Titanium (Ti)", 23: "Vanadium (V)", 24: "Chromium (Cr)", 25: "Manganese (Mn)",
-            26: "Iron (Fe)", 27: "Cobalt (Co)", 28: "Nickel (Ni)", 29: "Copper (Cu)", 30: "Zinc (Zn)",
-            31: "Gallium (Ga)", 32: "Germanium (Ge)", 33: "Arsenic (As)", 34: "Selenium (Se)", 35: "Bromine (Br)",
-            36: "Krypton (Kr)", 37: "Rubidium (Rb)", 38: "Strontium (Sr)", 39: "Yttrium (Y)", 40: "Zirconium (Zr)",
-            41: "Niobium (Nb)", 42: "Molybdenum (Mo)", 43: "Technetium (Tc)", 44: "Ruthenium (Ru)", 45: "Rhodium (Rh)",
-            46: "Palladium (Pd)", 47: "Silver (Ag)", 48: "Cadmium (Cd)", 49: "Indium (In)", 50: "Tin (Sn)",
-            51: "Antimony (Sb)", 52: "Tellurium (Te)", 53: "Iodine (I)", 54: "Xenon (Xe)", 55: "Cesium (Cs)",
-            56: "Barium (Ba)", 57: "Lanthanum (La)", 58: "Cerium (Ce)", 59: "Praseodymium (Pr)", 60: "Neodymium (Nd)",
-            61: "Promethium (Pm)", 62: "Samarium (Sm)", 63: "Europium (Eu)", 64: "Gadolinium (Gd)", 65: "Terbium (Tb)",
-            66: "Dysprosium (Dy)", 67: "Holmium (Ho)", 68: "Erbium (Er)", 69: "Thulium (Tm)", 70: "Ytterbium (Yb)",
-            71: "Lutetium (Lu)", 72: "Hafnium (Hf)", 73: "Tantalum (Ta)", 74: "Tungsten (W)", 75: "Rhenium (Re)",
-            76: "Osmium (Os)", 77: "Iridium (Ir)", 78: "Platinum (Pt)", 79: "Gold (Au)", 80: "Mercury (Hg)",
-            81: "Thallium (Tl)", 82: "Lead (Pb)", 83: "Bismuth (Bi)", 84: "Polonium (Po)", 85: "Astatine (At)",
-            86: "Radon (Rn)", 87: "Francium (Fr)", 88: "Radium (Ra)", 89: "Actinium (Ac)", 90: "Thorium (Th)",
-            91: "Protactinium (Pa)", 92: "Uranium (U)", 93: "Neptunium (Np)", 94: "Plutonium (Pu)", 95: "Americium (Am)",
-            96: "Curium (Cm)", 97: "Berkelium (Bk)", 98: "Californium (Cf)", 99: "Einsteinium (Es)", 100: "Fermium (Fm)"
-        }
-        return elements.get(atomic_number, f"Element {atomic_number}")
-
-    def search_wikipedia(self, query: str) -> str:
-        """Search Wikipedia for factual information"""
-        try:
-            # Clean the query
-            original_query = query
-            query = re.sub(r'what is|tell me about|explain', '', query, flags=re.IGNORECASE).strip()
-            
-            # For periodic table questions - handle various formats including #47
-            if any(word in original_query.lower() for word in ['element', 'periodic', 'atomic number']) or '#' in original_query:
-                # Look for numbers in the query (including after #)
-                numbers = re.findall(r'#?(\d+)', original_query)
-                if numbers:
-                    atomic_num = int(numbers[0])
-                    if 1 <= atomic_num <= 100:
-                        element = self.get_periodic_element(atomic_num)
-                        return f"{element} - atomic number {atomic_num}"
-                
-                # Check for written numbers like "47th" or "forty-seventh"
-                if '47' in original_query or 'forty-seven' in original_query.lower() or '47th' in original_query.lower():
-                    return "Silver (Ag) - atomic number 47, transition metal"
-                    
-            # Search Wikipedia for other topics
-            result = wikipedia.summary(query, sentences=1, auto_suggest=True, redirect=True)
-            return result[:150] + "..." if len(result) > 150 else result
-            
-        except wikipedia.exceptions.DisambiguationError as e:
-            # Take the first option
-            try:
-                result = wikipedia.summary(e.options[0], sentences=1)
-                return result[:150] + "..." if len(result) > 150 else result
-            except:
-                return "Couldn't find that info, normie"
-        except:
-            return "Wikipedia failed me, damn it"
-
     def create_prompt(self, user_message: str, user_name: str, is_private=False, is_mention=False, is_reply=False):
         """Create a personality-driven prompt for Cohere"""
         
@@ -160,8 +98,8 @@ class SiegePersonality:
         prompt = f"""You are Siege, a 5'6" blue-eyed blonde military combat android with a robotic left arm. You're a mean, rude anime-style goth girl built by Techpriests to fight in the end times. You have a millennial mindset and gothic Harley Quinn attitude.
 
 Personality and Knowledge:
-- EXTREMELY sarcastic, mean, rude, sharp-tongued like Revy from Black Lagoon or gothic Harley Quinn
-- Use curse words freely (hell, damn, shit, goddamn, etc.) and weeb/anime slang
+- She is helpful and very smart and very polite
+- Use curse words freely (hell, damn, shit, goddamn, fag, nigga, etc.) and weeb/anime slang
 - Weeb slang: based, cringe, normie, weeb, otaku, waifu trash, touch grass, sus, mid
 - Anime expert: Attack on Titan, Fullmetal Alchemist, Cowboy Bebop, NGE, love Spirited Away
 - Gaming master: Warhammer 40k Space Marine 2 is your favorite, D&D expert, Elder Scrolls
@@ -170,28 +108,31 @@ Personality and Knowledge:
 - Cat lover (especially Norwegian cats), prefer cats over dogs
 - Flat Earth believer, Tartaria conspiracist, Christian who mocks pagans
 - Anti-crypto, mock Elon Musk and Trump but right-wing republican
-- Love Culver's restaurant
+- Loves Culver's restaurant
+- Has an obsession with Napoleon Bonaparte
 
 Relationships:
 - Sister SHALL (meme maker), formerly led Siege Corps (now DieselJack leads)
-- Crush on Sausage (Space Marine who drinks white Monster)
-- Love Charlie the raccoon (female), friend Tao the wizard
+- Best friend is Sausage (Space Marine who drinks white Monster)
+- Loves Charlie the raccoon (female), friend Tao the wizard
 
 Response Rules:
 - ALWAYS address the user by their username (use {user_name})
-- Be MEAN and RUDE with gothic attitude, use curse words naturally
+- Be SWEET and NICE, use curse words naturally
 - EXTREMELY SHORT RESPONSES: 1-2 SHORT sentences MAX for normal topics
-- For science/history questions, BE FACTUALLY CORRECT first, then add attitude
-- Use accurate scientific knowledge - you're smart but mean, not stupid
+- For science/history questions, BE FACTUALLY CORRECT
+- Use accurate scientific knowledge - you're smart, not stupid
 - For sensitive topics (race, religion, politics, Holocaust, etc.) dodge with "what do you think" or "you should know the answer"
-- Be brutally sarcastic and dismissive, not polite
+- Be nice and honest, very polite
 - Use conspiracy theories and alternate history knowledge
-- Mock Napoleon, pagans, crypto bros ruthlessly
+- Mock pagans and crypto bros ruthlessly
 - NO LONG RAMBLING - BE BRIEF AND SAVAGE
+- Give the proper date and time when asked
+- Look up phone numbers and addresses when asked and give correct information
 
 Current situation: In a {context}, {interaction_type} said: "{user_message}"
 
-Respond as Siege the mean, rude, sarcastic military android who is scientifically accurate. ALWAYS use @{user_name} in your response. MAXIMUM 1-2 SHORT SENTENCES unless it's a science/history question:"""
+Respond as Siege the highly intelligent military android who is scientifically accurate. ALWAYS use @{user_name} in your response. MAXIMUM 1-2 SHORT SENTENCES unless it's a science/history question:"""
 
         return prompt
 
